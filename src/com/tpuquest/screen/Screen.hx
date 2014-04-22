e0c02561f86ebfa6ba39fc106e58471e3a859b0f
kazemir <mixcet@gmail.com>
2014-04-22 16:10:20 +0700

    add settings config

--- src/com/tpuquest/screen/Screen.hx
+++ src/com/tpuquest/screen/Screen.hx
@@ -6,6 +6,12 @@ import com.haxepunk.utils.Input;
 import com.haxepunk.utils.Key;
 import com.tpuquest.utils.DrawText;
 import haxe.Utf8;
+import flash.system.System;
+
+import haxe.xml.*;
+import sys.io.File;
+import sys.io.FileOutput;
+import sys.FileSystem;
 
 class Screen extends Scene
 {
@@ -24,12 +30,29 @@ class Screen extends Scene
 		Input.define("esc", [Key.ESCAPE]);
 		Input.define("action", [Key.ENTER, Key.X]);
 		
-		addGraphic(DrawText.CreateTextEntity("TPUQuest. Alpha v0.0.2", GameFont.Molot, 12, 5, 5, 0x0, false));
+		addGraphic(DrawText.CreateTextEntity("TPUQuest. Alpha v0.0.3", GameFont.Molot, 12, 5, 5, 0x0, false));
 		super.begin();
 	}
 	
 	public override function update()
 	{
 		super.update();
+	}
+	
+	private function ExitGame()
+	{
+		var config:Xml = Xml.createElement("settings");
+		if ( FileSystem.exists("config.xml") )
+		{
+			config.set("sound", Std.string(SettingsMenu.soudVolume));
+			config.set("music", Std.string(SettingsMenu.musicVolume));
+			config.set("language", SettingsMenu.language);
+			
+			var fout:FileOutput = File.write( "config.xml", false );
+			fout.writeString( config.toString() );
+			fout.close();
+		}
+		
+		System.exit(0);
 	}
 }
