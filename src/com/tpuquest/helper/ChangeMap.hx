17ba92930493058bba6ba1cba61833666920763d
konzhut <konzhut22@gmail.com>
2014-05-11 17:01:39 +0700

    Доработка хэлперов

--- src/com/tpuquest/helper/ChangeMap.hx
+++ src/com/tpuquest/helper/ChangeMap.hx
@@ -4,11 +4,16 @@ import com.tpuquest.utils.PointXY;
 
 class ChangeMap extends Helper
 {
-
-	public function new(point:PointXY, name:String="", show:Bool = false) 
+	public var nextMapPath:String;
+	public var keepPlayer:Bool;
+	public var instantly:Bool;
+	
+	public function new(point:PointXY, mapPath:String, currentPlayer:Bool, instantly:Bool, name:String="", show:Bool = false) 
 	{
 		super(point, name, show);
-		
+		nextMapPath = mapPath;
+		keepPlayer = currentPlayer;
+		this.instantly = instantly;
 	}
 	
 	public override function update()
