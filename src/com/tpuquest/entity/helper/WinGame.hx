3de24fa405d17e56292eea4bf60f0fd2f4c9cb37
konzhut <konzhut22@gmail.com>
2014-05-12 15:05:20 +0700

    Новые спрайты, новая иконка, возобновление работы тестов

---
+++ src/com/tpuquest/entity/helper/WinGame.hx
@@ -0,0 +1,17 @@
+package com.tpuquest.entity.helper;
+
+import com.tpuquest.utils.PointXY;
+
+class WinGame extends Helper
+{
+	
+	public function new(point:PointXY, name:String="", show:Bool = false) 
+	{
+		super(point, name, show);
+	}
+	
+	public override function update()
+	{
+		super.update();
+	}
+}
